3bbe87e0c5bb6025dc89351177240824fe1acd97
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:03:50 +1030

    Update C02-Ex05-ft_str_is_uppercase.c

--- Exercises/C02-Ex05-ft_str_is_uppercase.c
+++ Exercises/C02-Ex05-ft_str_is_uppercase.c
@@ -1,1 +1,24 @@
 
+#include <strings.h>
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if ((str[ctr] >= 65 && str[ctr] <= 90))
+			ctr++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+int main()
+{
+	printf("aaa: %d", ft_str_is_uppercase("GODHELPME"));
+}
+*/
