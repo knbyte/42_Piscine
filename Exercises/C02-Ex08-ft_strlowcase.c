733ba4a47ce04b2d6df73eeef115b9937b27f109
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:04:30 +1030

    Update C02-Ex08-ft_strlowcase.c

--- Exercises/C02-Ex08-ft_strlowcase.c
+++ Exercises/C02-Ex08-ft_strlowcase.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+char	*ft_strlowcase(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if (str[ctr] >= 65 && str[ctr] <= 90)
+			str[ctr] = str[ctr] + 32;
+		ctr++;
+	}
+	return (str);
+}
+
+/*
+int main ()
+{
+	char str[] = "abcde";
+	printf("%s/n", ft_strlowcase(str));
+	return 0;
+}
+*/
