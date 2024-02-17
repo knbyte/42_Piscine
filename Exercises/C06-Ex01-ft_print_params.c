c7a1b6439220d2c7207f3ea1fa37d1a7f9a39d51
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:11:56 +1030

    Update C06-Ex01-ft_print_params.c

--- Exercises/C06-Ex01-ft_print_params.c
+++ Exercises/C06-Ex01-ft_print_params.c
@@ -1,1 +1,32 @@
+#include <unistd.h>
 
+void	ft_putstr(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr])
+	{
+		write(1, &str[ctr++], 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	ctr;
+
+	ctr = 1;
+	while (ctr < argc)
+	{
+		ft_putstr(argv[ctr]);
+		ctr++;
+		write(1, "\n", 1);
+	}
+	return (0);
+}
+
+/*
+argc contains the number of arguments passed through the command line.
+
+argv[] is an array of character pointers, containing the parameters.
+*/
