92225bf209562ab32c7ce12850ae14b3f7ef8727
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:01:57 +1030

    Update C02-Ex03-ft_str_is_numeric.c

--- Exercises/C02-Ex03-ft_str_is_numeric.c
+++ Exercises/C02-Ex03-ft_str_is_numeric.c
@@ -1,1 +1,24 @@
 
+#include <strings.h>
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if ((str[ctr] >= 48 && str[ctr] <= 57))
+			ctr++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+int main()
+{
+	printf("aaa: %d", ft_str_is_numeric("12345"));
+}
+*/
