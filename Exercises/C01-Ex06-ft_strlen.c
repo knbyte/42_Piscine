f79a41af6f6d7ab6c256f6c1447af15031764cf0
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:59:31 +1030

    Update C01-Ex06-ft_strlen.c

--- Exercises/C01-Ex06-ft_strlen.c
+++ Exercises/C01-Ex06-ft_strlen.c
@@ -1,1 +1,23 @@
+#include <unistd.h>
+#include <strings.h>
+#include <stdio.h>
 
+int	ft_strlen(char	*str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr])
+		ctr++;
+	return (ctr);
+}
+
+/*
+int main()
+{
+    char str[] = "Counting string length";
+     int result = ft_strlen(str);
+     printf("The length of the string is %d.\n", result);
+    return 0;
+}
+*/
