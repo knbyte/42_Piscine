7840b7d4d9d22c55ffbb74de62cc377edabf45dc
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:57:05 +1030

    Update C00-Ex03-ft_print_numbers.c

--- Exercises/C00-Ex03-ft_print_numbers.c
+++ Exercises/C00-Ex03-ft_print_numbers.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
 
+void	ft_print_numbers(void)
+{
+	char	num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		write(1, &num, 1);
+		num++;
+	}
+}
+
+/*
+int main()
+{
+	ft_print_numbers();
+	return 0;
+}
+*/
