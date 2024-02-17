79ef33e3d25342d656248439479ca59d93916cf4
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:04:20 +1030

    Update C02-Ex07-ft_strupcase.c

--- Exercises/C02-Ex07-ft_strupcase.c
+++ Exercises/C02-Ex07-ft_strupcase.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if (str[ctr] >= 97 && str[ctr] <= 122)
+			str[ctr] = str[ctr] - 32;
+		ctr++;
+	}
+	return (str);
+}
+
+/*
+int main ()
+{
+	char str[] = "abcde";
+	printf("%s/n", ft_strupcase(str));
+	return 0;
+}
+*/
