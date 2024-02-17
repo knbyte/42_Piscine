11476610d41146c0fafb7b079973db3f751a8e00
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:00:56 +1030

    Update C02-Ex00-ft_strcpy.c

--- Exercises/C02-Ex00-ft_strcpy.c
+++ Exercises/C02-Ex00-ft_strcpy.c
@@ -1,1 +1,26 @@
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	ctr;
 
+	ctr = 0;
+	while (src[ctr] != '\0')
+	{
+		dest[ctr] = src[ctr];
+		ctr++;
+	}
+	dest[ctr] = '\0';
+	return (dest);
+}
+
+/*
+int main()
+{
+    char str1[] = "I am rather tired.";
+    char str2[] = "But it will be ok!";
+
+    printf("Before: %s\n", str1);
+    ft_strcpy(str1, str2);
+    printf("After: %s\n", str1);
+    return 0;
+}
+*/
