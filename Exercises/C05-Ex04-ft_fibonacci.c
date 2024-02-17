e691734ca055b6524f71a762bb5613ce5fe17c58
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:11:04 +1030

    Update C05-Ex04-ft_fibonacci.c

--- Exercises/C05-Ex04-ft_fibonacci.c
+++ Exercises/C05-Ex04-ft_fibonacci.c
@@ -1,24 +1,27 @@
-int ft_fibonacci(int index)
-{
-    int result;
+#include <stdio.h>
 
-    if  (index < 0)
-        return (-1);
-    if (index == 0 || index == 1)
-        return (index);
-    else
-    {
-        result = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
-        return (result);
-    }
+int	ft_fibonacci(int index)
+{
+	int	result;
+
+	if (index < 0)
+		return (-1);
+	if (index == 0 || index == 1)
+		return (index);
+	else
+	{
+		result = ft_fibonacci(index -1) + ft_fibonacci(index - 2);
+		return (result);
+	}
 }
 
 #ifdef DO_MAIN
-int    main(void)
-{
-    int    result;
 
-    result = ft_fibonacci(6);
-    printf("%d", result);
+int	main(void)
+{
+	int	result;
+
+	result = ft_fibonacci(6);
+	printf("%d", result);
 }
 #endif
