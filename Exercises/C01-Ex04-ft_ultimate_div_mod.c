bc66e1261fdb022268cb1c37b3989435e32bca27
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:58:42 +1030

    Update C01-Ex04-ft_ultimate_div_mod.c

--- Exercises/C01-Ex04-ft_ultimate_div_mod.c
+++ Exercises/C01-Ex04-ft_ultimate_div_mod.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+
+	div = *a / *b;
+	*b = *a % *b;
+	*a = div;
+}
+
+/*
+int main()
+{
+    int a;
+    int b;
+    
+    a = 16;
+    b = 5;
+    printf("a: %d, b: %d", a, b);
+    ft_ultimate_div_mod(&a, &b);
+    printf("a: %d, b: %d", a, b);
+}
+*/
