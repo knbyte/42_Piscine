3e2e68ff22e57caa83bd099ec5fc1f5627c41221
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:57:41 +1030

    Update C00-Ex04-ft_is_negative.c

--- Exercises/C00-Ex04-ft_is_negative.c
+++ Exercises/C00-Ex04-ft_is_negative.c
@@ -1,1 +1,24 @@
 
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+	{
+		write(1, "P", 1);
+	}
+	else
+	{
+		write(1, "N", 1);
+	}
+}
+
+/*
+int main()
+{
+	int i;
+	i = 100;
+	ft_is_negative(i);
+	return 0;
+}
+*/
