b53172c9a6d0f61e87d71d3ac34ba85a7cf37558
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:01:07 +1030

    Update C02-Ex01-ft_strncpy.c

--- Exercises/C02-Ex01-ft_strncpy.c
+++ Exercises/C02-Ex01-ft_strncpy.c
@@ -1,1 +1,33 @@
+#include <strings.h>
+#include <stdio.h>
 
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	ctr;
+
+	ctr = 0;
+	while (src[ctr] != '\0' && ctr < n)
+	{
+		dest[ctr] = src[ctr];
+		ctr++;
+	}
+	while (ctr < n)
+	{
+		dest[ctr] = '\0';
+		ctr++;
+	}
+	return (dest);
+}
+
+/*
+int    main(void)
+{
+    char    src[] = "hello";
+    char    dest[] = "hi";
+
+    printf("Src: %s\n", src);
+    printf("Before: %s\n", dest);
+    ft_strncpy(dest, src, 5);
+    printf("After: %s\n", dest);
+}
+*/
