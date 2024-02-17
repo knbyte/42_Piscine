75b23871de23934a6a9c5e1740376d5541c72300
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:01:44 +1030

    Update C02-Ex02-ft_str_is_alpha.c

--- Exercises/C02-Ex02-ft_str_is_alpha.c
+++ Exercises/C02-Ex02-ft_str_is_alpha.c
@@ -1,1 +1,25 @@
+#include <strings.h>
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if ((str[ctr] >= 65 && str[ctr] <= 90) || \
+				(str[ctr] >= 97 && str[ctr] <= 122))
+			ctr++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+int main()
+{
+	printf("aaa: %d", ft_str_is_alpha("hello"));
+}
+*/
 
