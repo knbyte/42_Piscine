638661d41cec97d0f6ae23cbd96f8917b036434e
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 17:04:05 +1030

    Update C02-Ex06-ft_str_is_printable.c

--- Exercises/C02-Ex06-ft_str_is_printable.c
+++ Exercises/C02-Ex06-ft_str_is_printable.c
@@ -1,1 +1,24 @@
 
+#include <strings.h>
+#include <stdio.h>
+
+int	ft_str_is_printable(char *str)
+{
+	int	ctr;
+
+	ctr = 0;
+	while (str[ctr] != '\0')
+	{
+		if ((str[ctr] >= ' ' && str[ctr] <= '~'))
+			ctr++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+int main ()
+{
+	printf("aaa: %d", ft_str_is_printable("lalalalala"));
+}
+*/
