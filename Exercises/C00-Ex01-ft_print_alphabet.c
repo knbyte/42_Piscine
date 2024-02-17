b107002b7b197308807ad8b980c4830f7598cc6f
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:55:41 +1030

    Update C00-Ex01-ft_print_alphabet.c

--- Exercises/C00-Ex01-ft_print_alphabet.c
+++ Exercises/C00-Ex01-ft_print_alphabet.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
 
+void	ft_print_alphabet(void)
+{
+	char	abc;
+
+	abc = 'a';
+	while (abc <= 'z')
+	{
+		write(1, &abc, 1);
+		abc++;
+	}
+}
+
+/*
+int main()
+{
+	ft_print_alphabet();
+	return 0;
+}
+*/
