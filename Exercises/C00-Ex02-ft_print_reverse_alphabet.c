e23159e61d28ae1e44320c346aefe84c4f39c92f
Aydan <160217940+knbyte@users.noreply.github.com>
2024-02-17 16:56:21 +1030

    Update C00-Ex02-ft_print_reverse_alphabet.c

--- Exercises/C00-Ex02-ft_print_reverse_alphabet.c
+++ Exercises/C00-Ex02-ft_print_reverse_alphabet.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
 
+void	ft_print_reverse_alphabet(void)
+{
+	char	abc;
+
+	abc = 'z';
+	while (abc >= 'a')
+	{
+		write(1, &abc, 1);
+		abc--;
+	}
+}
+
+/*
+int main()
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}
+*/
